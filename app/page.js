94dd328dc905dd20114962d63c21f43ec78c0866
lewisgoing <lgoing7@uw.edu>
2024-05-30 17:35:42 -0700

    added cursor

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 import styles from './page.module.css'
 import { useEffect, useRef, useState } from 'react';
 import Link from "next/link";
-// import AnimatedCursor from 'animated-cursor';
+import AnimatedCursor from 'animated-cursor';
 
 // Icons
 import { FaApple } from "react-icons/fa";
@@ -68,50 +68,67 @@ export default function Index() {
     return images;
   }
   //
-  // useEffect(() => {
-  //   const cursorOptions = {
-  //       innerSize: 1,
-  //     cursorInnerStyles: {
-  //       backgroundColor: 'var(--cursor-color)', // white
-  //       mixBlendMode: 'exclusion'
-  //     },
-  //     cursorOuterStyles: {
-  //       backgroundColor: 'var(--cursor-color)', // white
-  //       mixBlendMode: 'exclusion'
-  //     }
-  //   };
-  //   const cursor = AnimatedCursor(cursorOptions);
-  //   cursor.init();
-  // }, []);
+
+
+  // for cursor
+  useEffect(() => {
+    const cursorOptions = {
+      outerAlpha: 0,
+      cursorInnerStyles: {
+        backgroundColor: '#000000',
+        mixBlendMode: 'exclusion'
+      },
+      cursorOuterStyles: {
+        backgroundColor: '#fff', // white
+        mixBlendMode: 'exclusion'
+      }
+    };
+    const cursor = AnimatedCursor(cursorOptions);
+    cursor.init();
+  }, []);
 
   return (
-      <main className={styles.main}>
+      <main className={styles.main} >
 
-        <div onMouseMove={(e) => manageMouseMove(e)} className={styles.imageCursorSpace}>
+        <div onMouseMove={(e) => manageMouseMove(e)} className={styles.imageCursorSpace}  > {/* add id="cursor" for logo glitch alone with commented out code below*/}
           {
             [...Array(33).keys()].map((_, index) => {
               const ref = useRef(null);
               refs.push(ref);
-              return <img key={index} onClick={() => console.log(refs)} ref={ref} src={`/images/${index}.jpg`}/>;
+              return <img id="cursor" key={index} onClick={() => console.log(refs)} ref={ref} src={`/images/${index}.jpg`}/>;
             })
           }
-          <div className={styles.textBox}>
-            <span ref={textRef} className={styles.floatingText}>lewisgoing</span>
-            <span className={styles.subheader}>a multimedia project</span>
+          <div className={styles.textBox} >
+            {/*<div className={styles.textBox} id="cursor-inner">*/}
+            <div >
+              <span ref={textRef} className={styles.floatingText} >lewisgoing</span>
+              <span className={styles.subheader}>a multimedia project</span>
+            </div>
 
           </div>
-          <div className={styles.navPanel}>
+          <div className={styles.navPanel} >
 
 
-            <Link ref={textRef} className={styles.navItem} href={'https://soundcloud.com/lewisgoing'}>soundcloud</Link>{"\n"}
-            <Link ref={textRef} className={styles.navItem} href={'https://open.spotify.com/artist/0Ll3UtLjep87qvzadZ2Cp4?si=kbOPgrmRQJ-nz1kS3dquQg'}>spotify</Link>{"\n"}
-            <Link ref={textRef} className={styles.navItem} href={'https://music.apple.com/us/artist/lewisgoing/1563419779'}>apple music </Link>{"\n"}
+            <Link ref={textRef} className={styles.navItem}
+                  href={'https://soundcloud.com/lewisgoing'}>soundcloud</Link>{"\n"}
+            <Link ref={textRef} className={styles.navItem}
+                  href={'https://open.spotify.com/artist/0Ll3UtLjep87qvzadZ2Cp4?si=kbOPgrmRQJ-nz1kS3dquQg'}>spotify</Link>{"\n"}
+            <Link ref={textRef} className={styles.navItem}
+                  href={'https://music.apple.com/us/artist/lewisgoing/1563419779'}>apple music </Link>{"\n"}
             {/*<FaApple size={20} />*/}
-            <Link ref={textRef} className={styles.navItem} href={'https://soundcloud.com/earth2lewis'}>livesets</Link>{"\n"}
-            <Link ref={textRef} className={styles.navItem} href={'mailto:whereislewisgoing@gmail.com'}>booking</Link>{"\n"}
+            <Link ref={textRef} className={styles.navItem}
+                  href={'https://soundcloud.com/earth2lewis'}>livesets</Link>{"\n"}
+            <Link ref={textRef} className={styles.navItem}
+                  href={'mailto:whereislewisgoing@gmail.com'}>booking</Link>{"\n"}
 
           </div>
+          <div id="cursor">
+            <div id="cursor-inner"></div>
+            <div id="cursor-outer"></div>
+          </div>
+
         </div>
+
 
         <footer className={styles.footer}>
           Copyright 2024 © lewisgoing
