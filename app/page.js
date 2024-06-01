ea6ab75a4df3a6db510e1b7653e8e48856c9250e
lewisgoing <lgoing7@uw.edu>
2024-05-31 18:49:33 -0700

    added click fx

--- app/page.js
+++ app/page.js
@@ -3,10 +3,10 @@ import styles from './page.module.css'
 import { useEffect, useRef, useState } from 'react';
 import Link from "next/link";
 import AnimatedCursor from 'animated-cursor';
+import classNames from 'classnames'; // Import classNames library
 
 // Icons
 import { FaApple } from "react-icons/fa";
-
 
 export default function Index() {
   let steps = 0;
@@ -15,6 +15,7 @@ export default function Index() {
   let maxNumberOfImages = 8;
   let refs = [];
   const textRef = useRef(null);
+  const [isInverted, setIsInverted] = useState(false); // State to manage color inversion
 
   const manageMouseMove = (e) => {
     const { clientX, clientY, movementX, movementY } = e;
@@ -23,12 +24,12 @@ export default function Index() {
     if (steps >= currentIndex * 150) {
       moveImage(clientX, clientY);
 
-      if (nbOfImages == maxNumberOfImages) {
+      if (nbOfImages === maxNumberOfImages) {
         removeImage();
       }
     }
 
-    if (currentIndex == refs.length) {
+    if (currentIndex === refs.length) {
       currentIndex = 0;
       steps = -150;
     }
@@ -67,10 +68,18 @@ export default function Index() {
     }
     return images;
   }
-  //
 
+  const handleClick = (ref) => {
+    ref.current.classList.add(styles.clicked);
+    setTimeout(() => {
+      ref.current.classList.remove(styles.clicked);
+    }, 500);
+  }
 
-  // for cursor
+  const handleInvertColors = () => {
+    setIsInverted(!isInverted);
+  }
+
   useEffect(() => {
     const cursorOptions = {
       outerAlpha: 0,
@@ -79,7 +88,7 @@ export default function Index() {
         mixBlendMode: 'exclusion'
       },
       cursorOuterStyles: {
-        backgroundColor: '#fff', // white
+        backgroundColor: '#fff',
         mixBlendMode: 'exclusion',
         outerSize: 1,
       },
@@ -90,48 +99,33 @@ export default function Index() {
   }, []);
 
   return (
-      <main className={styles.main} >
-
-        <div onMouseMove={(e) => manageMouseMove(e)} className={styles.imageCursorSpace}  > {/* add id="cursor" for logo glitch alone with commented out code below*/}
+      <main className={styles.main}>
+        <div onMouseMove={(e) => manageMouseMove(e)}  className={classNames(styles.imageCursorSpace, { [styles.inverted]: isInverted })} onClick={handleInvertColors}>
           {
             [...Array(33).keys()].map((_, index) => {
               const ref = useRef(null);
               refs.push(ref);
-              return <img id="cursor" key={index} onClick={() => console.log(refs)} ref={ref} src={`/images/${index}.jpg`}/>;
+              return <img key={index} onClick={() => handleClick(ref)} ref={ref} src={`/images/${index}.jpg`} />;
             })
           }
           <div className={styles.textBox} >
-            {/*<div className={styles.textBox} id="cursor-inner">*/}
             <div >
               <span ref={textRef} className={styles.floatingText} >lewisgoing</span>
               <span className={styles.subheader}>a multimedia project</span>
             </div>
-
           </div>
           <div className={styles.navPanel} >
-
-
-            <Link ref={textRef} className={styles.navItem}
-                  href={'https://soundcloud.com/lewisgoing'}>soundcloud</Link>{"\n"}
-            <Link ref={textRef} className={styles.navItem}
-                  href={'https://open.spotify.com/artist/0Ll3UtLjep87qvzadZ2Cp4?si=kbOPgrmRQJ-nz1kS3dquQg'}>spotify</Link>{"\n"}
-            <Link ref={textRef} className={styles.navItem}
-                  href={'https://music.apple.com/us/artist/lewisgoing/1563419779'}>apple music </Link>{"\n"}
-            {/*<FaApple size={20} />*/}
-            <Link ref={textRef} className={styles.navItem}
-                  href={'https://soundcloud.com/earth2lewis'}>livesets</Link>{"\n"}
-            <Link ref={textRef} className={styles.navItem}
-                  href={'mailto:whereislewisgoing@gmail.com'}>booking</Link>{"\n"}
-
+            <Link ref={textRef} className={styles.navItem} href={'https://soundcloud.com/lewisgoing'}>soundcloud</Link>{"\n"}
+            <Link ref={textRef} className={styles.navItem} href={'https://open.spotify.com/artist/0Ll3UtLjep87qvzadZ2Cp4?si=kbOPgrmRQJ-nz1kS3dquQg'}>spotify</Link>{"\n"}
+            <Link ref={textRef} className={styles.navItem} href={'https://music.apple.com/us/artist/lewisgoing/1563419779'}>apple music </Link>{"\n"}
+            <Link ref={textRef} className={styles.navItem} href={'https://soundcloud.com/earth2lewis'}>livesets</Link>{"\n"}
+            <Link ref={textRef} className={styles.navItem} href={'mailto:whereislewisgoing@gmail.com'}>booking</Link>{"\n"}
           </div>
           <div id="cursor" >
             <div id="cursor-inner" className={styles.cursorInner}></div>
             <div id="cursor-outer" className={styles.cursorOuter}></div>
           </div>
-
         </div>
-
-
         <footer className={styles.footer}>
           Copyright 2024 © lewisgoing
         </footer>
